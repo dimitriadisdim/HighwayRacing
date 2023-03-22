d243594eb4c47e473137878b336bc0560c291f1c
kilkistanproductions <ddimitr@uth.gr>
2023-03-22 14:26:05 +0200

    Camera

--- Scripts/Level.cs
+++ Scripts/Level.cs
@@ -3,12 +3,12 @@ using System;
 
 public class Level : Node
 {
-    private Camera _camera;
+    private Camera2D _camera;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        _camera = GetNode<Camera>("Player/Camera2D");
+        _camera = GetNode<Camera2D>("Player/Camera2D");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
