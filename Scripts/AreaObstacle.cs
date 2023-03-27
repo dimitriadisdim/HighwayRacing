e4730018a9ac7c6d907d9f3f88a222d7f000660f
kilkistanproductions <ddimitr@uth.gr>
2023-03-27 14:27:09 +0300

    Fix movement and player collision detection

---
+++ Scripts/AreaObstacle.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class AreaObstacle : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	
+	private void OnArea2DBodyEntered(object body)
+	{
+		var body2d = body as Node2D;
+        body2d.QueueFree();
+	}
+}
+
+
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,10 +5,11 @@ using System.Diagnostics;
 public class Player : KinematicBody2D
 {
 	//Movement
-	[Export] private int _spd;
 	[Export] private int _spdVertical;
+	[Export] private int _spd;
 	private int _currentLane; // 0-4 Lanes
 	private int[] _lanes;
+	private bool _right;
 	
 
 	// Called when the node enters the scene tree for the first time.
@@ -19,9 +20,10 @@ public class Player : KinematicBody2D
 		{
 			200, 300, 415, 520
 		};
+		_spdVertical = 1500;
 		_currentLane = 0;
 		_spd = 500;
-		_spdVertical = 1500;
+		_right = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,7 +35,8 @@ public class Player : KinematicBody2D
 		var angle = GetAngleTo(pos);
 		var motion = Vector2.Zero; 
 		//Calculate angle
-		motion.x = Mathf.Cos(angle) * _spd;
+		if((_right && angle == 0) || (!_right && angle == Mathf.Pi))
+			motion.x = Mathf.Cos(angle) * _spd;
 		//Add y axis 
 		motion.y = -_spdVertical;
 		//Apply motion
@@ -54,9 +57,13 @@ public class Player : KinematicBody2D
 		motion = motion.Normalized();
 	  
 		//Change lane
-		if (motion.x > 0 && _currentLane != 3)
+		if (motion.x > 0 && _currentLane != 3){
+			_right = true;
 			_currentLane++;
-		if (motion.x < 0 && _currentLane != 0)
+		}
+		if (motion.x < 0 && _currentLane != 0){
+			_right = false;
 			_currentLane--;
+		}
 	}
 }
